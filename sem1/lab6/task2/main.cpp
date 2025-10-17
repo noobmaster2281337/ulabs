1d7fc072ff863ac818468894b2fa030fb9989e2d
noobmaster2281337 <markvoronov8@mail.ru>
2025-10-17 21:51:16 +0200

    add: таск2

--- sem1/lab6/task2/main.cpp
+++ sem1/lab6/task2/main.cpp
@@ -1,36 +1,61 @@
 #include <iostream>
-
 using namespace std;
+void fillmas(int* mas, int length) {
+	for (int i = 0; i < length; i++) {
+		cin >> mas[i];
+	}
+}
+void printmas(int* mas, int length) {
+	for (int i = 0; i < length; i++) {
+		cout << mas[i] << " ";
+	}
+	cout << endl;
+}
+int sum_of_digits(int x) {
+	int sum = 0;
+	while (x > 0) {
+		sum += x % 10;
+		x /= 10;
+	}
+	return sum;
+}
+int mult_of_digits(int x) {
+	int mult = 1;
+	while (x > 0) {
+		mult = mult * (x % 10);
+		x /= 10;
+	}
+	return mult;
+}
+void sortmasbysumofdigits(int* mas, int length) {
+	for (int i = 0; i < length; i++) {
+		for (int j = i; j < length; j++) {
+			if (sum_of_digits(mas[i]) < sum_of_digits(mas[j])) continue;
+			else if (sum_of_digits(mas[i]) > sum_of_digits(mas[j])) swap(mas[i], mas[j]);
+			else {
+				if (mult_of_digits(mas[i]) < mult_of_digits(mas[j])) continue;
+				else if (mult_of_digits(mas[i]) > mult_of_digits(mas[j])) swap(mas[i], mas[j]);
+				else {
+					if (mas[i] > mas[j]) swap(mas[i], mas[j]);
+				}
+			}
+		}
+	}
+}
 int main()
 {
-    
 	setlocale(LC_ALL, "Ru");
-	// 1
-    int n; // количество чисел последовательности
-    cin >> n;
-    int sum = 0, maxnegative = INT_MIN, index = 0;
-    for (int i = 0; i < n; i++) {
-        int num; 
-        cin >> num;
-        if ((num < 0) && (num % 7 == 0)) {
-            sum += num;
-            if (num > maxnegative) {
-                maxnegative = num;
-                index = i + 1;
-            }
-        }
-    }
-    cout << "Сумма: " << sum << endl;
-    cout << "Наибольшее число: " << maxnegative << endl;
-    cout << "Номер последовательности: " << index << endl;
-    // в задаче не указан случай если нет отрицательных чисел/кратных семи
-    // 2
-    int chislo, proiz = 1;
-    cin >> chislo;
-    while (chislo > 0) {
-        proiz = proiz * (chislo % 10);
-        chislo = chislo/10;
-    }
-    cout << proiz;
-    return 0;
+	int maslength = 0;
+	cout << "Введите длину последовательности: " << endl;
+	cin >> maslength;
+
+	int* firstmas = new int[maslength];
+	cout << endl << "Введите последовательность: " << endl;
+	fillmas(firstmas, maslength);
+
+	sortmasbysumofdigits(firstmas, maslength);
+
+	printmas(firstmas, maslength);
+	delete[] firstmas;
+	return 0;
 }
