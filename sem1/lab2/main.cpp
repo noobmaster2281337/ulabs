952de98d5ae13cdfdd7e584109f4203b2ba79eee
noobmaster2281337 <markvoronov8@mail.ru>
2025-10-17 20:28:39 +0200

    add: lab2

--- sem1/lab2/main.cpp
+++ sem1/lab2/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+using namespace std;
 int main()
 {
-	
+	// 6 Вариант
+	setlocale(LC_ALL, "Ru");
+	int S, h;
+	double V;
+	cout << "Введите площадь основания пирамиды" << endl;
+	cin >> S;
+	cout << "Введите высоту пирамиды" << endl;
+	cin >> h;
+	if (S >= 1 || S <= 99) {
+		if (h >= 1 || h <= 99)
+			V = (S * h) / 3.0;
+		cout << "Объём пирамиды: " << V;
+	}
+	else
+		cout << "Введены недопустимые значения";
+	return 0;
 }
