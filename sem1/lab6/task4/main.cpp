7d72448b9bfc199184b45af4d2632dff812b5098
noobmaster2281337 <markvoronov8@mail.ru>
2025-10-17 22:44:13 +0200

    add: task4

--- sem1/lab6/task4/main.cpp
+++ sem1/lab6/task4/main.cpp
@@ -1,36 +1,67 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+
+bool is_prime(int x) {
+    int r = sqrt(x); // целой части хватит
+    for (int d = 2; d <= r; d++)
+        if (x % d == 0) {
+            return false; // простое
+        }
+    return true; // не простое
+}
+bool is_cube(int x) {
+    int y = exp(log(x) / 3);
+    if (y * y * y == x) return true;
+    return false;
+}
+
 int main()
 {
-    
 	setlocale(LC_ALL, "Ru");
-	// 1
-    int n; // количество чисел последовательности
+    int n;
     cin >> n;
-    int sum = 0, maxnegative = INT_MIN, index = 0;
+
+    // Инициализируем массив
+    int* A = new int[2 * n];
     for (int i = 0; i < n; i++) {
-        int num; 
-        cin >> num;
-        if ((num < 0) && (num % 7 == 0)) {
-            sum += num;
-            if (num > maxnegative) {
-                maxnegative = num;
-                index = i + 1;
+        cin >> A[i]; // Читаем элементы массива
+    }
+    // Удаляем элементы из массива
+    for (int i = 0; i < n;) { // i++ не надо
+        if (is_prime(A[i])) {
+            // Сдвигаем элементы влево
+            for (int j = i + 1; j < n; j++) {
+                A[j - 1] = A[j];
             }
+            n--; // Уменьшаем размер массива
+        }
+        else {
+            i++; // Переходим к следующему элементу
         }
     }
-    cout << "Сумма: " << sum << endl;
-    cout << "Наибольшее число: " << maxnegative << endl;
-    cout << "Номер последовательности: " << index << endl;
-    // в задаче не указан случай если нет отрицательных чисел/кратных семи
-    // 2
-    int chislo, proiz = 1;
-    cin >> chislo;
-    while (chislo > 0) {
-        proiz = proiz * (chislo % 10);
-        chislo = chislo/10;
+    // Дублирование
+    for (int i = 0; i < n;) {
+        if (is_cube(A[i])) {
+            // Увеличиваем размер массива
+            n++;
+            for (int j = n - 1; j > i + 1; j--) {
+                A[j] = A[j - 1]; // Сдвигаем элементы вправо
+            }
+            A[i + 1] = A[i]; // Дублируем элемент
+            i += 2; // Пропускаем следующий дубликат
+        }
+        else {
+            i++; // Переходим к следующему элементу
+        }
     }
-    cout << proiz;
+    
+    // Вывод результата
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    delete[] A; // Освобождаем память
     return 0;
 }
