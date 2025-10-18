62a4560103633ab021e64b5d8895afa0c2a2604b
noobmaster2281337 <markvoronov8@mail.ru>
2025-10-18 04:05:11 +0200

    add: lab10

--- sem1/lab10/main.cpp
+++ sem1/lab10/main.cpp
@@ -1,81 +1,189 @@
 #include <iostream>
-
 using namespace std;
-int main()
+
+struct Node {   
+    Node* next;
+    int data;
+};
+
+
+void makeList(Node* head, int n) 
 {
-	// Переменная - хранилище значения определённого типа
-	
-	// Целочисленный тип
-	cout << "Type name: int" << endl;
-	cout << "Size: " << sizeof(int) << endl;
-	cout << "Min: " << INT_MIN << endl;
-	cout << "Max: " << INT_MAX << endl;
-	int a, b;
-	a = -7; b = 9;
-	cout << "a + b = " << a + b << endl;
-	cout << endl;
-	cout << "Type name: unsigned int" << endl;
-	cout << "Size: " << sizeof(int) << endl;
-	cout << "Min: " << 0 << endl;
-	cout << "Max: " << UINT_MAX << endl;
-	unsigned int c, d;
-	c = 3; d = 4;
-	cout << "c + d = " << c + d << endl;
-	cout << endl;
-	cout << "Type name: short" << endl;
-	cout << "Size: " << sizeof(short) << endl;
-	cout << "Min: " << SHRT_MIN << endl;
-	cout << "Max: " << SHRT_MAX << endl;
-	short e, f;
-	e = -2; f = 4;
-	cout << "e + f = " << e + f << endl;
-	cout << endl;
-	cout << "Type name: unsigned short" << endl;
-	cout << "Size: " << sizeof(short) << endl;
-	cout << "Min: " << 0 << endl;
-	cout << "Max: " << USHRT_MAX << endl;
-	unsigned short g, h;
-	g = 3; h = 4;
-	cout << "g + h = " << g + h << endl;
-	cout << endl;
-	cout << "Type name: long" << endl;
-	cout << "Size: " << sizeof(long) << endl;
-	cout << "Min: " << LONG_MIN << endl;
-	cout << "Max: " << LONG_MAX << endl;
-	long i, j;
-	i = -7802; j = 98345;
-	cout << "i + j = " << i + j << endl;
-	cout << endl;
-	cout << "Type name: unsigned long" << endl;
-	cout << "Size: " << sizeof(long) << endl;
-	cout << "Min: " << 0 << endl;
-	cout << "Max: " << ULONG_MAX << endl;
-	unsigned long k, l;
-	k = 972353; l = 28374;
-	cout << "k + l = " << k + l << endl;
-	cout << endl;
-	// Вещественный тип
-	cout << "Type name: double" << endl;
-	cout << "Size: " << sizeof(double) << endl;
-	cout << "Min: " << DBL_MIN << endl;
-	cout << "Max: " << DBL_MAX << endl;
-	double m, n;
-	m = 34.7; n = 4.9;
-	cout << "m + n = " << m + n << endl;
-	cout << endl;
-	// Символьный тип
-	cout << "Type name: char" << endl;
-	cout << "Size: " << sizeof(char) << endl;
-	cout << "Min: " << CHAR_MIN << endl;
-	cout << "Max: " << CHAR_MAX << endl;
-	char o;
-	o = 'w';
-	cout << "o = " << o << endl;
-	cout << endl;
-	// Логический тип
-	cout << "Type name: bool" << endl;
-	cout << "Size: " << sizeof(bool) << endl;
-	cout << "Min: " << false << endl;
-	cout << "Max: " << true << endl;
-	return 0;
+    Node* p = head;
+    for (int i = 0; i < n; i++)
+    {
+        Node* q = new Node;
+        p->next = q;
+
+        cout << "Enter a value: ";
+        cin >> q->data;
+        q->next = nullptr;
+
+        p = p->next;
+    }
+}
+
+
+void printList(Node* head) 
+{
+    Node* p = head;
+    while (p->next != nullptr)
+    {
+        cout << p->next->data << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+
+
+void clearList(Node* head) 
+{
+    Node* p = head;
+    while (p->next != nullptr)
+    {
+        Node* tmp = p->next;
+        p->next = p->next->next;
+        delete tmp;
+    }
+    delete p;
+}
+
+
+bool containsIdenticalDigits(int n) 
+{
+    n = abs(n);
+
+    for (int i = 0; i < 10; i++)
+    {
+        int counter = 0;
+        int number = n;
+        while (number != 0)
+        {
+            if (number % 10 == i)
+            {
+                counter++;
+            }
+            number /= 10;
+        }
+        if (counter > 1) return true;
+    }
+
+    return false;
+}
+
+
+bool doesListContainsNumberWithIdenticalDigits(Node* head) 
+{
+    Node* p = head;
+    while (p->next != nullptr)
+    {
+        if (containsIdenticalDigits(p->next->data))
+        {
+            return true;
+        }
+        p = p->next;
+    }
+    return false;
+}
+
+
+int getLastDigit(int n) 
+{
+    return abs(n) % 10;
+}
+
+
+int getFirstDigit(int n) 
+{
+    while (n > 9)
+    {
+        n /= 10;
+    }
+    return n;
+}
+
+
+void removeElemFromList(Node* head, Node* elem) 
+{
+    Node* p = head;
+    while (p->next != nullptr)
+    {
+        if (p->next == elem)
+        {
+            Node* tmp = p->next;
+            p->next = p->next->next;
+            delete tmp;
+            return;
+        }
+        p = p->next;
+    }
+}
+
+
+void copyElemFromList(Node* head, Node* elem) 
+{
+    Node* p = head;
+    while (p->next != nullptr)
+    {
+        if (p->next == elem)
+        {
+            Node* q = new Node;
+            q->data = p->next->data;
+            q->next = p->next->next;
+            p->next->next = q;  
+            return;
+        }
+        p = p->next;
+    }
+}
+
+
+/* Ввести последовательность натуральных чисел. Если в последовательности нет ни одного числа, содержащего две одинаковых цифры, 
+ упорядочить последовательность по невозрастанию последней цифры числа. В противном случае удалить из последовательности числа, 
+ начинающиеся четной цифрой и продублировать остальные числа. Последовательность хранить в односвязном списке.*/
+
+
+int main() 
+{
+    Node* head = new Node{nullptr, 0};
+    int n;
+    cout << "Enter an array length: ";
+    cin >> n;
+    makeList(head,  n);
+    
+    if (!doesListContainsNumberWithIdenticalDigits(head))
+    {
+        Node* p = head->next;
+        for (Node * a = p; a->next != nullptr; a = a->next)
+            for (Node * b = a->next; ; b = b->next)
+            {
+                if (getLastDigit(a->data) < getLastDigit(b->data)) swap(a->data, b->data);
+                if (b->next == nullptr) break;
+            }
+    }
+    else
+    {
+        Node* p = head;
+        while (p->next != nullptr)
+        {
+            if ((getFirstDigit(p->next->data)) % 2 == 0)
+            {
+                removeElemFromList(head, p->next);
+            }
+            else
+            {
+                p = p->next;
+            } 
+        }
+        p = head;
+        while (p->next != nullptr)
+        {
+            copyElemFromList(head, p->next);
+            p = p->next->next;
+        }
+    }
+    
+    printList(head);
+    clearList(head);
+    return 0;
 }
